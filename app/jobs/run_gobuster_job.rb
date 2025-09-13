299ac444c314792bbac2b2e9ee3da0132b7912db
Yates <yates0x00@sina.com>
2025-09-13 07:20:15 +0000

    增加了gobuster 检测脚本 app/jobs/run_gobuster_job.rb

---
+++ app/jobs/run_gobuster_job.rb
@@ -0,0 +1,19 @@
+class RunGobusterJob < ApplicationJob
+  queue_as :default
+
+  def perform(server)
+    result = ''
+    result += `gobuster dir -u #{server.protocal}://#{server.name} -w /opt/app/SecLists/dir_file.txt -t 50 -b 403,404 --random-agent`
+    result += `gobuster dir -u #{server.protocal}://#{server.name} -w /opt/app/SecLists/Discovery/Web-Content/raft-small-files-lowercase.txt -t 50 -b 403,404 --random-agent`
+    result += `gobuster dir -u #{server.protocal}://#{server.name} -w /opt/app/SecLists/Discovery/Web-Content/raft-small-directories-lowercase.txt -t 50 -b 403,404 --random-agent`
+
+    server.update(
+      is_detected_by_gobuster: true,
+      gobuster_result: result
+    )
+  rescue => e
+    Rails.logger.error "== RunGobusterJob failed for #{server.name}: #{e.message}"
+    raise  # 触发重试机制
+  end
+end
+
