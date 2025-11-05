9d18e06b45f71e41e6997609e27c46478f77f887
Yates <yates0x00@sina.com>
2025-11-05 23:48:58 +0000

    继续优化 view页面

--- app/models/project.rb
+++ app/models/project.rb
@@ -4,12 +4,19 @@ class Project < ApplicationRecord
   has_many :c_class_networks
 
   def ips_not_behind_waf
-    result = []
-    servers.includes(:ips).where('is_confirmed_not_behind_waf = 1 and level <= 3').find_each do |server|
-      server.ips.each do |ip_entity|
-        result << ip_entity
+
+    count = servers.includes(:ips).where('is_confirmed_not_behind_waf = 1 and level <= 3').count
+
+    if count <= 1000
+      result = []
+      servers.includes(:ips).where('is_confirmed_not_behind_waf = 1 and level <= 3').find_each do |server|
+        server.ips.each do |ip_entity|
+          result << ip_entity
+        end
       end
+      return result.sort_by { |ip_entity| ip_entity.ip }
+    else
+      return ["ip count: #{count}"]
     end
-    return result.sort_by { |ip_entity| ip_entity.ip }
   end
 end
